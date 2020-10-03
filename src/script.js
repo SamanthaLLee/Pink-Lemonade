4caaa6267dc2812f916e072a0a14becbb24b41a3
Samantha Lee <35175439+SamanthaLLee@users.noreply.github.com>
2020-10-03 15:42:16 -0400

    Changed input type to button
    It's more compatible with the API call

--- src/script.js
+++ src/script.js
@@ -2,14 +2,14 @@ var response = [];
 
 function getRecipe(q){
 	console.log(q);
-	$.ajax({
-		url:"https://api.spoonacular.com/recipes/complexSearch?apiKey=99a01e3d7525405894929bfbec77ffa3&addRecipeInformation=true&addRecipeNutrition=true&number=1&query="+q,
-		success: function(res) {
-			console.log(res.results[0]);
-			//document.getElementById("output").innerHTML="<h1>"+res.results[0].title+"</h1><br><img src='"+res.results[0].image+"' width='400' /><br>Ready in "+res.results[0].readyInMinutes+" minutes"
-			//getSource(res.results[0].id)
-		}
-	});
+	// $.ajax({
+	// 	url:"https://api.spoonacular.com/recipes/complexSearch?apiKey=99a01e3d7525405894929bfbec77ffa3&addRecipeInformation=true&addRecipeNutrition=true&number=1&query="+q,
+	// 	success: function(res) {
+	// 		console.log(res.results[0]);
+	// 		//document.getElementById("output").innerHTML="<h1>"+res.results[0].title+"</h1><br><img src='"+res.results[0].image+"' width='400' /><br>Ready in "+res.results[0].readyInMinutes+" minutes"
+	// 		//getSource(res.results[0].id)
+	// 	}
+	// });
 }
 
 function getSource(id){
