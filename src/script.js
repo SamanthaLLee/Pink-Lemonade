98ac100cdedf51eb418d5e710460f68f4fb50ee1
Samantha Lee <35175439+SamanthaLLee@users.noreply.github.com>
2020-10-06 18:18:23 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,28 +1,30 @@
 var response = [];
-var ingredients = [];
-var resultsToDisplay = [];
+const ingredients = [];
+const resultsToDisplay = [];
 
 //Food Conditions
-var goodFoods = ["Broccoli", "Cauliflower", "Kale", "Cabbage", "Brussel Sprouts", "Lettuce", "Spinach", "Swiss Chard", "Endives","Beet greens", "Romaine", "Turnip", "Kohlrabi", "Bok Choy",
-"Watercress", "Collards", "Kale", "Mustard Greens", "Rutabaga", "Celery", "Parsley", "Fennel", "Carrots", "Parsnip", "Garlic", "Onion", "Shallots", "Chives", "Leek", "Egglant",
-"Tomato", "Pumpkin", "Squah", "Cucumber", "Muskmelon", "Watermelon", "Broccoli Sprouts", "Mustard Greens", "Horseradish", "Potatos", "Apple", "Pear", "Arugula", "Oranges", "Grapefruit",
-"Lemons", "Limes", "Tangerines", "Strawberries", "Blueberries", "Peaches", "Fortified Soymilk", "Yogurt", "Lentils", "Poultry", "Soybeans", "Peas", "Chickpeas", "Lima Beans", "Peanut",
+const goodFoods = ["Broccoli", "Cauliflower", "Kale", "Cabbage", "Brussel Sprouts", "Lettuce", "Spinach", "Swiss Chard", "Endives","Beet greens", "Romaine", "Turnip", "Kohlrabi", "Bok Choy",
+"Watercress", "Collards", "Kale", "Mustard Greens", "Rutabaga", "Celery", "Parsley", "Fennel", "Carrots", "Parsnip", "Garlic", "Onion", "Shallots", "Chives", "Leek", "Eggplant",
+"Tomato", "Pumpkin", "Squash", "Cucumber", "Muskmelon", "Watermelon", "Broccoli Sprouts", "Mustard Greens", "Horseradish", "Potatoes", "Apple", "Pear", "Arugula", "Oranges", "Grapefruit",
+"Lemons", "Limes", "Tangerines", "Strawberries", "Blueberries", "Peaches", "Yogurt", "Lentils", "Poultry", "Peas", "Chickpeas", "Lima Beans", "Peanut",
 "Carob", "Kidney Beans", "Mung beans", "Pinto Beans", "Black-eyed Peas", "Eggs", "Tofu", "Tempeh", "Edamame", "Wheat", "Rye", "Oats", "Rice", "Corn", "Bulgur", "Barley", "Green tea",
-"Kimchi", "Miso", "Sauerkraut", "Parsley", "Rosemary", "Thyme", "Tumeric", "Ginger", "Soy", "Ground Flaxseeds", "Salmon", "Mackerel", "Sardines", "Artic Char"];
+"Kimchi", "Miso", "Sauerkraut", "Parsley", "Rosemary", "Thyme", "Tumeric", "Ginger", "Ground Flaxseeds", "Salmon", "Mackerel", "Sardines", "Artic Char"];
 
-var badFoods = ["Cheeses", "Cream", "Butter", "Ice cream", "Beef", "Lamb", "Organ Meats", "Hydrogenated Oils", "Olive Oil", "Avocado", "Nuts", "Seeds", "Baked Goods", "Crackers",
+const badFoods = ["Cheeses", "Cream", "Butter", "Ice cream", "Beef", "Lamb", "Organ Meats", "Hydrogenated Oils", "Olive Oil", "Avocado", "Nuts", "Seeds", "Baked Goods", "Crackers",
 "Margarine", "Alcohol", "Hydrogenated Oils", "Sugary beverages", "Tobacco Smoke", "Artificial Sweetners", "Cider", "Spirits", "Molluscs", "Fried foods", "Fast Foods"]
 		
+const questionable = ["Soy","Soybeans", "Fortified Soymilk" ]
 		
 //Makes a call to the API with user's query, returns an array of recipes that match
 function getRecipe(q){
 	console.log(q);
 	$.ajax({
-			//Edit endpoint below to modify call
-		url:"https://api.spoonacular.com/recipes/complexSearch?apiKey=99a01e3d7525405894929bfbec77ffa3&addRecipeInformation=true&addRecipeNutrition=true&number=1&query="+q,
+		//Edit endpoint below to modify call
+		url:"https://api.spoonacular.com/recipes/complexSearch?apiKey=99a01e3d7525405894929bfbec77ffa3&addRecipeInformation=true&addRecipeNutrition=true&number=10&query="+q,
 		success: function(res) {
 			response = res;
-			console.log(res.results[0]);
+			console.log(res.results);
+			rankResults(res);
 			//document.getElementById("output").innerHTML="<h1>"+res.results[0].title+"</h1><br><img src='"+res.results[0].image+"' width='400' /><br>Ready in "+res.results[0].readyInMinutes+" minutes"
 			//getSource(res.results[0].id);
 		}
@@ -49,12 +51,71 @@ function rankResults(response){
 	// add recipe object WITH SCORE to resultsToDisplay
 	// sort resultsToDisplay based on score
 	
+	response.forEach(function(element) {
+  	element.score = 0;
+	})
+
+	for(var i = 0 in response){
+		// var ingredObject = getIngredients(response[i]);
+		// response[i].score = ingredObject.score;
+		response[i].score = getIngredients(response[i]);
+		resultsToDisplay.push(response[i]);
+	}
+
+	// resultsToDisplay.sort(function(a,b) {
+	// 	return b.score-a.score;
+	// });
+	
+	console.log(resultsToDisplay);
+	
 }
 
 //Gets list of ingredients from a given recipe
 function getIngredients(recipe){
-	// we don't need to call the api here, just parse recipe
-	// suggestions: apply point system here and return a number
+	var score = 0;
+	var result = [];
+	var goodIngredients = [];
+	var badIngredients = [];
+	var questionableIngred = [];
+	var ingredientList = recipe.ingredients;
+	for(var i = 0 in ingredientList){
+		if(goodFoods.includes(ingredientList[i])){
+			score++;
+			goodIngredients.push(ingredientList[i].name);
+		}else if(badFoods.includes(ingredientList[i])){
+			score--;
+			badIngredients.push(ingredientList[i].name);
+		}else if(questionable.includes(ingredientList[i])){
+			questionableIngred.push(ingredientList[i].name);
+		}
+		
+		result.score = score;
+		// 
+ 		// var max = Math.max(goodIngredients.length,badIngredients.length,questionableIngred.length);
+		// 
+		// if(max == goodIngredients.length){
+		// 	result.foods = goodIngredients;
+		// 	result.category = "G";
+		// 	return result;
+		// }else if(max == badIngredients.length){
+		// 	result.foods = badIngredients;
+		// 	result.category = "B";
+		// 	return result;
+		// }else{
+		// 	result.foods = questionableIngred;
+		// 	result.category = "NA";
+		// 	return result;
+		// }
+
+		// var master = [];
+		// master.push(goodIngredients);
+		// master.push(badIngredients);
+		// master.push(questionableIngred);
+		
+		console.log(score);
+		
+		return score;
+	}
 }
 
 //Displays results, post-pointing
